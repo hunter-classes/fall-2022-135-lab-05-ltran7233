b850c032d9811193389a7853a6e24ac9bb195b4a
ltran7233 <ltran7233@LAPTOP-AAAUJLF3.localdomain>
2022-10-06 17:29:11 -0400

    added task A function and tests in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
-
 #include "funcs.h"
 
 int main()
 {
-  return 0;
+	std::cout << "isDivisibleBy(100, 25) is " << isDivisibleBy(100, 25) << std::endl;
+	std::cout << "isDivisibleBy(35, 17) is " << isDivisibleBy(35, 17) << std::endl;
+	
+	return 0;
 }
