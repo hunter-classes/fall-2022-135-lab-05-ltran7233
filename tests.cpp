37edf865dea04ed162fb5850f3308190dee2a1cb
ltran7233 <ltran7233@LAPTOP-AAAUJLF3.localdomain>
2022-10-06 17:49:03 -0400

    added tests for taskA

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
 #include "funcs.h"
 
-// add your tests here
+TEST_CASE("isDivisibleBy Tests")
+{
+	CHECK(isDivisibleBy(20, 5) == true);
+	CHECK(isDivisibleBy(-15, 3) == true);
+	CHECK(isDivisibleBy(-100, -2) == true);
+	CHECK(isDivisibleBy(87, 6) == false);
+	CHECK(isDivisibleBy(34, 7) == false);
+}
